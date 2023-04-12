f705f4cba2d567f699975f2698bcaeebcd737609
inpsyde-deploybot <github-deploybot@inpsyde.com>
2023-04-12 07:13:49 +0000

    [SYNC] [FAU-214] Conditional fields validation (#137)

--- resources/ts/util/transformedErrorMessages.ts
+++ resources/ts/util/transformedErrorMessages.ts
@@ -4,12 +4,14 @@ import { DegreeProgramDataPaths } from 'defs';
 
 const transformedErrorMessages: Partial<Record<DegreeProgramDataPaths, string>> = {
     'featured_image.id': __('Please select a featured image.', 'fau-degree-program'),
+    'teaser_image.id': __('Please select a teaser image.', 'fau-degree-program'),
 };
 
 const transformedErrorMessagesBasedOnErrorCode: Partial<Record<string, string>> = {
     rest_too_short: __('This field can not be empty.', 'fau-degree-program'),
     rest_too_short_en: __('English variant of this field can not be empty.', 'fau-degree-program'),
     rest_too_few_items: __('Please select at least one item.', 'fau-degree-program'),
+    rest_invalid_pattern: __('Field does not match pattern.', 'fau-degree-program'),
 };
 
 export { transformedErrorMessages, transformedErrorMessagesBasedOnErrorCode };
