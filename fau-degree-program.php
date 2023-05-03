eee8658b36412a55812156f805fcf1760dfdcd16
inpsyde-deploybot <github-deploybot@inpsyde.com>
2023-05-03 14:30:08 +0000

    [SYNC] Version bump (1.0.0)
    First officially stable release.

--- fau-degree-program.php
+++ fau-degree-program.php
@@ -4,7 +4,7 @@
  * Plugin Name: FAU Degree Program Management
  * Plugin URI:  https://github.com/RRZE-Webteam/FAU-Studium
  * Description: Add and edit degree programs and expose them via the REST API.
- * Version:     0.0.13
+ * Version:     1.0.0
  * Author:      Inpsyde GmbH
  * Author URI:  https://inpsyde.com/
  * Update URI:  false
