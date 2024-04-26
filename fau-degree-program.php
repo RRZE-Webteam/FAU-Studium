83911d0b4d59b7f3217e71957c67ca0c138d9a38
Amin <amin.pe94@gmail.com>
2024-04-26 21:08:05 +0200

    chore: update package name

--- fau-degree-program.php
+++ fau-degree-program.php
@@ -5,8 +5,8 @@
  * Plugin URI:  https://github.com/RRZE-Webteam/FAU-Studium
  * Description: Add and edit degree programs and expose them via the REST API.
  * Version:     1.2.7
- * Author:      Inpsyde GmbH
- * Author URI:  https://inpsyde.com/
+ * Author:      Syde GmbH
+ * Author URI:  https://syde.com/
  * Update URI:  false
  * GitHub Plugin URI: RRZE-Webteam/FAU-Studium
  * License:     GPL-2.0-or-later
