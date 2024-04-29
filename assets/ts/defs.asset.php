e0d5f5085db42967e55a81661825fad478e995fa
Amin <amin.pe94@gmail.com>
2024-04-29 13:45:59 +0200

    chore: remove assets and vendor directory from vcs

--- assets/ts/defs.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '78f3342d7545ce368566');
--- assets/ts/degree-program-list-table.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'fb47a5d043850d6a232e');
