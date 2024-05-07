0d5913e677404809f47ec5b3df41d7d34b6579f3
Amin <amin.pe94@gmail.com>
2024-05-07 10:33:34 +0200

    feat: add validation messages display on frontend and backend

--- src/Infrastructure/Dashboard/TermMeta/TermMetaRegistrar.php
+++ src/Infrastructure/Dashboard/TermMeta/TermMetaRegistrar.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Fau\DegreeProgram\Infrastructure\Dashboard\TermMeta;
 
+use Fau\DegreeProgram\Application\FormFieldValidation\FormFieldValidationMessages;
 use Fau\DegreeProgram\Common\Infrastructure\TemplateRenderer\Renderer;
+use WP_Error;
 use WP_Term;
 
 final class TermMetaRegistrar
@@ -12,6 +14,7 @@ final class TermMetaRegistrar
     public function __construct(
         private Renderer $termMetaFieldRenderer,
         private TermMetaRepository $termMetaRepository,
+        private FormFieldValidationMessages $validationMessages,
     ) {
     }
 
@@ -31,6 +34,11 @@ final class TermMetaRegistrar
                 $taxonomy,
                 $field->key(),
                 $field->metaArgs()
+            );
+
+            add_filter(
+                "fau.degree_programs.taxonnomy_{$taxonomy}_term_metas.validation_rules",
+                fn (array $rules) => $this->addJavascriptFieldValidationRules($rules, $field),
             );
         }
 
@@ -64,21 +72,83 @@ final class TermMetaRegistrar
 
         add_action("edit_{$taxonomy}", $updateCallback);
         add_action("create_{$taxonomy}", $updateCallback);
+        add_filter('pre_insert_term', $this->buildValidationCallback(...$termMetaFields));
+    }
+
+    // phpcs:ignore Inpsyde.CodeQuality.NestingLevel.High
+    private function buildValidationCallback(TermMetaField ...$termMetaFields): callable
+    {
+        return function (mixed $term) use ($termMetaFields): mixed {
+            if ($term instanceof WP_Error) {
+                return $term;
+            }
+
+            $error = null;
+
+            foreach ($termMetaFields as $termMetaField) {
+                $postedValue = filter_input(
+                    INPUT_POST,
+                    $termMetaField->key(),
+                    FILTER_SANITIZE_SPECIAL_CHARS
+                );
+
+                $error = $this->checkForErrors($termMetaField, $postedValue);
+
+                if ($error instanceof WP_Error) {
+                    return $error;
+                }
+            }
+
+            return $term;
+        };
+    }
+
+    private function checkForErrors(TermMetaField $termMetaField, mixed $value): ?WP_Error
+    {
+        foreach ($termMetaField->validationRules() as $validationRule) {
+            if ($validationRule->passes($value)) {
+                continue;
+            }
+
+            return new WP_Error(
+                'invalid_term_meta',
+                $this->validationMessages->get($validationRule, $termMetaField->title()),
+            );
+        }
+
+        return null;
     }
 
     private function buildUpdateCallback(TermMetaField ...$termMetaFields): callable
     {
         return function (int $termId) use ($termMetaFields): void {
             foreach ($termMetaFields as $termMetaField) {
-                // phpcs:ignore WordPressVIPMinimum.Security.PHPFilterFunctions.MissingThirdParameter
                 $postedValue = filter_input(
                     INPUT_POST,
                     $termMetaField->key(),
+                    FILTER_SANITIZE_SPECIAL_CHARS
                 );
 
                 $sanitizedValue = $termMetaField->sanitize($postedValue);
                 $this->termMetaRepository->update($termId, $termMetaField->key(), $sanitizedValue);
             }
         };
+    }
+
+    private function addJavascriptFieldValidationRules(array $rules, TermMetaField $field): array
+    {
+        $result = [];
+
+        foreach ($field->validationRules() as $validationRule) {
+            $result[] = (object) [
+                'name' => $validationRule::name(),
+                'payload' => $validationRule->payload(),
+                'errorMessage' => $this->validationMessages->get($validationRule, $field->title()),
+            ];
+        }
+
+        $rules[$field->key()] = $result;
+
+        return $rules;
     }
 }
