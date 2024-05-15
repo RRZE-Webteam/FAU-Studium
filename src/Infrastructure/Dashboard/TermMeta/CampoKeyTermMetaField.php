8fdd294822bb8c92880ef0afb494c12432539d9f
Amin <amin.pe94@gmail.com>
2024-05-15 21:12:39 +0200

    refactor: use campokey meta key name from common package

--- src/Infrastructure/Dashboard/TermMeta/CampoKeyTermMetaField.php
+++ src/Infrastructure/Dashboard/TermMeta/CampoKeyTermMetaField.php
@@ -3,18 +3,17 @@
 declare(strict_types=1);
 
 namespace Fau\DegreeProgram\Infrastructure\Dashboard\TermMeta;
+use Fau\DegreeProgram\Common\Infrastructure\Repository\CampoKeysRepository;
 
 class CampoKeyTermMetaField extends InputTermMetaField
 {
-    public const KEY = 'uniquename';
-
     public function __construct(
         string $description,
         TermMetaFieldValidationPattern $validationPattern = null,
     ) {
 
         parent::__construct(
-            self::KEY,
+            CampoKeysRepository::CAMPOKEY_TERM_META_KEY,
             __('Campo Key', 'fau-degree-program'),
             $description,
             validationPattern: $validationPattern,
