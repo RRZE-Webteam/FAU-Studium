0279adbdd6642fcdc286a9388f9c3b6ac10071e0
Oleksandr Zhyian <oleksandr.zhyian@gmail.com>
2024-08-02 14:01:55 +0300

    fix: degree program revisions

--- src/Infrastructure/Repository/CacheBasedRevisionRepository.php
+++ src/Infrastructure/Repository/CacheBasedRevisionRepository.php
@@ -156,7 +156,7 @@ final class CacheBasedRevisionRepository implements DegreeProgramRevisionReposit
                 DegreeProgram::COMBINATIONS => $this->combinationsToContextualIdList($rawRevision->combinations()),
                 DegreeProgram::LIMITED_COMBINATIONS => $this->combinationsToContextualIdList($rawRevision->limitedCombinations()),
                 DegreeProgram::APPLY_NOW_LINK => $this->structureToContextualId($rawRevision->applyNowLink()),
-                DegreeProgram::CAMPO_KEYS => $rawRevision->campoKeys()->asArray(),
+                DegreeProgram::CAMPO_KEYS => implode(', ', $rawRevision->campoKeys()->asArray()),
             ],
             self::multilingualStringToFlatArray(DegreeProgram::ENTRY_TEXT, $rawRevision->entryText()),
         );
